a8dbccceb5bc4e54291d117a0cc812c54bb48ce6
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-17 07:41:40 -0800

    Commit: Printing int variable using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - print single digit numbers starting from '0'
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
