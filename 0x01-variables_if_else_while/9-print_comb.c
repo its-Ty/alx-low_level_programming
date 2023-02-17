7402f573ef246be5c17fcf73d943754a5a17139d
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-17 08:20:35 -0800

    Commit: Print single digit numbers seperated by commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - Print single digit numbers separated with commas
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
