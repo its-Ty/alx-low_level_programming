ab309d638882916337531a095d4daef98cf3af8c
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-21 11:02:22 -0800

    Commit: print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: variable being checked
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	_putchar('0' + (n % 10));
+
+	return (n % 10);
+}
