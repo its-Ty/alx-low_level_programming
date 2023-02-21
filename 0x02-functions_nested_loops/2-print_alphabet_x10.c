70e1b4b17eb87df52c3f7c16416c306c4b69d671
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-21 10:12:49 -0800

    Commit: Print alphabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - Prints lowercase alphabet ten times followed by a new line
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter = 'a';
+	
+	for ( int i = 1; i < 11; i++)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	}
+	_putchar('\n');
+}
