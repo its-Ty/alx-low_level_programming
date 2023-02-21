204ceb53af3cd653407ac53edc3a43bea23a9cbc
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-21 11:16:58 -0800

    Commit: print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * jack_bauer - print every minute of a day
- * Description: print minutes 00:00 to 23:59
+ * jack_bauer - print every minute of the day
+ * Description: print every minute from 00:00 to 23:59
  */
 
 void jack_bauer(void)
