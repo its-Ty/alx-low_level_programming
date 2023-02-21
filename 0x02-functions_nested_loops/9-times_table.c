dff63162a240191e5ff61b1b48a2802fe875cd9e
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-21 11:37:08 -0800

    Commit: Nine Times Table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * times_table - print nine times table
+ */
+
+void times_table(void)
+{
+	int r;
+	int c;
+	int p = (r * c);
+
+	for (r = 0; r <= 9; r++)
+	{
+		for (c = 0; c <= 9; c++)
+		{
+			if (c == 0)
+			{
+				_putchar('0' + p);
+			}
+			else if (p <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + p);
+			}
+			else if (p > 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (p / 10));
+				_putchar('0' + (p % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
