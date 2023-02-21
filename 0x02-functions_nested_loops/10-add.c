22cb78847ecf1adbfe32e925acbb43536a3c46f3
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-21 11:44:34 -0800

    Commit: Return sum of 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: first number
+ * @b: second number
+ * Return: sum of both numbers
+ */
+
+int add(int a, int b)
+{
+	int sum = (a + b);
+	return (sum);
+}
