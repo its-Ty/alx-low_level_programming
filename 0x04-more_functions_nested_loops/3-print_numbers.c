8db8a8dc17e18dbefe7c057ae9640ad1b6bbe544
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-23 00:46:57 -0800

    Commit: print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - Print numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		_putchar(num);
+	}
+
+	_putchar('\n');
+}
