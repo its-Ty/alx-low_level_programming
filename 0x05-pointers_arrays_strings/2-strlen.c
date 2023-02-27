3fe318647b91fe3d6bd3d11576ec456eb752ba21
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-27 05:48:01 -0800

    Commit: get length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns length of string
+ * @s: character of string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int length;
+
+	while (*s != '\0')
+	{
+		length++;
+		*s++;
+	}
+
+	return (length);
+}
