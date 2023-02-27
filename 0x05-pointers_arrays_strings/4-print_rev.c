65687f8755a1eccea3fd12af1b9cb7f0d69a0348
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-27 06:15:25 -0800

    Commit: Print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse
+ * followed by a new line
+ * @s: string to reverse
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	s--;
+
+	while (length > 0)
+	{
+		_putchar(*s);
+		s--;
+		length--;
+	}
+
+	_putchar('\n');
+}
