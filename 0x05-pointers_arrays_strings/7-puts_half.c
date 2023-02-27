c7cac10188a3f853d424dc308b69dafe5d9c9e2b
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-27 07:51:35 -0800

    Commit: Print half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - prints half of the string
+ * @str: string to print
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int len;
+
+	if (_strlen(str) % 2 != 0)
+	{
+		len = (_strlen(str)-1) / 2;
+	}
+	else
+	{
+		len = _strlen(str) / 2;
+	}
+
+	for (i = len; i < _strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
