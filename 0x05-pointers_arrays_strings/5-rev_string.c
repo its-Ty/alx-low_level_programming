523638127ff239e7c4609ad3f966beb59e99e0b8
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-02-27 07:09:57 -0800

    Commit: Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * rev_string: reverses string
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int countOne = 0;
+	int countTwo = (_strlen(s) - 1);
+	char temp;
+
+	while (countOne < countTwo)
+	{
+		temp = s[countOne];
+		s[countOne] = s[countTwo];
+		s[countTwo] = temp;
+		countOne++;
+		countTwo--;
+	}
+}
