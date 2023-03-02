110fc6d47d1f78ca99b77bb283ab4b57129dbc03
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-02 04:16:21 -0800

    Commit: Reverse Array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+/*
  * reverse_array - reverse array elements
  * @a: array
  * @n: number of elements in array
@@ -8,16 +8,21 @@
 
 void reverse_array(int *a, int n)
 {
-	int temp = 0;
-	int i = 0;
-	int j = n - 1;
+	int i;
+	int j;
+	int temp;
+
+	i= 0;
+	j = n - 1;
+	temp = 0;
 
 	while (i < j)
 	{
-		temp = *(a + i);
-		*(a + i) = *(a + j);
-		*(a + j) = temp;
-		beg++;
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+
+		i++;
 		j--;
 	}
 }
