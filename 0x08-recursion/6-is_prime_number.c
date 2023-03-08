01ef5c8b6c680b19fb9a8dbd700eb32dc4700a8f
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-08 09:51:05 -0800

    Commit: Prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * isPrime - recursively divide while skipping even numbers
+ * @n: number to check
+ * @divisor: divisor
+ * Return: 1 if prime, 0 if not, or recursive function call
+ */
+
+int is_prime(int n, int divisor)
+{
+	if (n == divisor)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+	return (is_prime(n, divisor + 1));
+}
+
+/**
+ * is_prime_number - check if number is a prime
+ * @n: number to check
+ * Return: 1 if prime, 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	int divisor = 3;
+
+	if (n % 2 == 0 || n < 2)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (is_prime(n, divisor));
+}
