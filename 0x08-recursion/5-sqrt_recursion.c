1645eb81fbcf380ebf058ff619b529545565c548
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-08 09:42:38 -0800

    Commit: natural root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * find_root - find square root of n, starting from the smallest possible, 0
- * @n: n
+ * get_sqRoot - find square root of n
+ * @n: number
  * @root: test this root
  * Return: natural square root, or -1 if not natural root
  */
