494762314dc34b5a5cd1e738ea92531b86d94cea
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-08 03:37:48 -0800

    Commit: Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line
+ * _print_rev_recursion - Prints a string in reverse
  * @s: String to print
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	_puts_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
