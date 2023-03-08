f9ea448ea645776bc2a1dbc35cd8a38b4fbcba4e
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-08 09:40:38 -0800

    Commit: natural root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,37 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: the number to find the square root of
- * Return: the square root of n, or -1 if n does not have a natural square root
+ * find_root - find square root of n, starting from the smallest possible, 0
+ * @n: n
+ * @root: test this root
+ * Return: natural square root, or -1 if not natural root
+ */
+
+int get_sqRoot(int n, int root)
+{
+	if (root * root > n)
+	{
+		return (-1);
+	}
+
+	if (root * root == n)
+	{
+		return (root);
+	}
+	return (get_sqRoot(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
  */
 
 int _sqrt_recursion(int n)
 {
-	int root;
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-	else
-	{
-		root = 1;
-		while (root * root <= n)
-		{
-			if (root * root == n)
-			{
-				return (root);
-			}
-			root++;
-		}
-		return (-1);
-	}
+	return (get_sqRoot(n, 0));
 }
