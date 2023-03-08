31607f7c63d32a8eff323045ccf09cf49ea42cc9
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-08 10:04:26 -0800

    Commit: Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,17 +7,17 @@
  * Return: 1 if prime, 0 if not, or recursive function call
  */
 
-int is_prime(int n, int divisor)
+int isPrime(int n, int divisor)
 {
-        if (n == divisor)
-        {
-                return (1);
-        }
-        if (n % divisor == 0)
-        {
-                return (0);
-        }
-        return (is_prime(n, divisor + 1));
+	if (n == divisor)
+	{
+		return (1);
+	}
+	if (n % divisor == 0)
+	{
+		return (0);
+	}
+        return (isPrime(n, divisor + 1));
 }
 
 /**
@@ -28,15 +28,14 @@ int is_prime(int n, int divisor)
 
 int is_prime_number(int n)
 {
-        int divisor = 3;
-
-        if (n % 2 == 0 || n < 2)
-        {
-                return (0);
-        }
-        if (n == 2)
-        {
-                return (1);
-        }
-        return (is_prime(n, divisor));
+	int divisor = 3;
+	if (n % 2 == 0 || n < 2)
+	{
+		return (0);
+	}
+	if (n == 2)
+	{
+		return (1);
+	}
+	return (is_prime(n, divisor));
 }
