efa8fcb91054d625ab2672945862589551513158
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-08 03:55:30 -0800

    Commit: Return length of string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,23 +3,16 @@
 /**
  * _strlen_recursion - returns length of string
  * @s: string to get length of
+ * Return: string length
  */
 
 int _strlen_recursion(char *s)
 {
-	int count;
-
-        count = 0;
-
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 
-	while (*s != '\0')
-	{
-		count++;
-		s++;
-	}
-	return (count);
+	s++;
+	return (1 + _strlen_recursion(s));
 }
