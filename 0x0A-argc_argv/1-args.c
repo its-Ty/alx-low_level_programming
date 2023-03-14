b21946efb067c908f29b693ff8b68a510081eed7
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-14 05:56:43 -0700

    Check number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * main - print number of arguments
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%d\n", argc - 1);
+	}
+
+	return (0);
+}
