11614e1801a0c047ba2f612d2f126dd8c1c93f27
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-16 10:06:18 -0700

    Concatenate Two Strings Update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,10 +13,7 @@ int _strlen(char *string)
 
 	i = 0;
 	while (string[i] < '\0')
-	{
 		i++;
-	}
-		;
 	return (i);
 }
 
@@ -36,38 +33,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	num = n;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 	if (num < 0)
-	{
 		return (NULL);
-	}
 	if (num >= _strlen(s2))
-	{
 		num = _strlen(s2);
-	}
 
 	length = _strlen(s1) + num + 1;
 
 	point = malloc(sizeof(*point) * length);
 	if (point == NULL)
-	{
 		return (NULL);
-	}
 
 	for (c1 = 0; s1[c1] != '\0'; c1++)
-	{
 		point[c1] = s1[c1];
-	}
 	for (c2 = 0; c2 < num; c2++)
-	{
 		point[c1 + c2] = s2[c2];
-	}
 	point[c1 + c2] = '\0';
 
 	return (point);
