585fdf2f57298239ac9c34bb83bfab25e7ffc68f
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-16 09:55:47 -0700

    Concatenate two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,74 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _strlen - calculate length length
+ * @string: string
+ * Return: string length
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	i = 0;
+	while (string[i] < '\0')
+	{
+		i++;
+	}
+		;
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *point;
+	int num, length, c1, c2;
+
+	num = n;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (num < 0)
+	{
+		return (NULL);
+	}
+	if (num >= _strlen(s2))
+	{
+		num = _strlen(s2);
+	}
+
+	length = _strlen(s1) + num + 1;
+
+	point = malloc(sizeof(*point) * length);
+	if (point == NULL)
+	{
+		return (NULL);
+	}
+
+	for (c1 = 0; s1[c1] != '\0'; c1++)
+	{
+		point[c1] = s1[c1];
+	}
+	for (c2 = 0; c2 < num; c2++)
+	{
+		point[c1 + c2] = s2[c2];
+	}
+	point[c1 + c2] = '\0';
+
+	return (point);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * _calloc - Set values to 0 and allocate memory
+ * @nmemb: size
+ * @size: sizeof(datatype)
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *point;
+	unsigned int count; /* match unsigned arguments */
+
+	if (nmemb <= 0 || size <= 0)
+	{
+		return (NULL);
+	}
+
+	point = malloc(nmemb * size);
+	if (point == NULL)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; count < (nmemb * size); count++)
+	{
+		*((char *)point + count) = 0;
+	}
+
+	return (point);
+}
