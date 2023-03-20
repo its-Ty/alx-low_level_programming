ef45ff1cb8755cba6e8feaf565abf0e04802d03b
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-20 10:08:24 -0700

    dog header file - update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,9 @@ typedef struct dog dog_t;
  */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
