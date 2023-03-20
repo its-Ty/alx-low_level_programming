2ecfc9ce38ee0a22a41afb5487471b2c852accaf
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-20 10:22:09 -0700

    initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include "stdio.h"
+
+/*
+ * init_dog - initialize a variable of type struct dog
+ * @d: variable of type struct dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
