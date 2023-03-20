5a3150d6fcc7b775c1ba288b4b2cb6147f686bc5
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-20 10:53:59 -0700

    Free a dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+#include "stdio.h"
+
+/**
+ * new_dog - Creates new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner name
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
