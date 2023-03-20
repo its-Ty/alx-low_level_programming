54fa014f5b1fd7711c4c9b5a11e33461ea066dc5
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-20 10:03:55 -0700

    dog header file - update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - Typedef for struct dog
+ */
+
 typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dog data
+ * @name: dog name data
+ * @age: dog age value
+ * @owner: dog owner name
+ */
 
 struct dog
 {
 	char* name;
-	char* owner;
 	float age;
+	char* owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
