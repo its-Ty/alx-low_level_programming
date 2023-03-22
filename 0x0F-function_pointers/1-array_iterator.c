7b034ae2485a24d2570cdf935371992e863f2581
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-22 09:19:32 -0700

    Array Iterator - updated

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int counter;
 
 	for (counter = 0; counter < size; counter++)
-        {
-                action(array[counter]);
-        }
+	{
+		action(array[counter]);
+	}
 }
