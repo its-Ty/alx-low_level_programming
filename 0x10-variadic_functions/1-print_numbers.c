6628751742027ae1f71facc03818dd83f799fd1a
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-23 05:26:49 -0700

    Print numbers - updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -18,10 +18,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-	}
-	if (separator != NULL && i < n - 1)
-	{
-		printf("%s", separator);
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
 	}
 	va_end(args);
 
