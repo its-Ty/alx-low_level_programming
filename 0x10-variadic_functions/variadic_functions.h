752e40e93d0bb64df029bf639c75c1c1f4fbeb37
Tyheel Pragraj <tyheelpragraj19@gmail.com>
2023-03-24 06:23:18 -0700

    Print anything

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,17 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+/**
+ * struct dt - struct
+ * @letter: letter signifying data type
+ * @func: function ptr
+ */
+typedef struct dt
+{
+	char letter;
+	void (*func)(va_list);
+} datatype;
+
 int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
